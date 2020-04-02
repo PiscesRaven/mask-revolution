c693dd22e63cb26125d40ff38baf07ad63bceb89
Reaven <y93209@gmail.com>
2020-04-02 18:23:08 +0800

    base main

--- src/view/Main.vue
+++ src/view/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-    <MenuContainer :Tw_Stores="Tw_Stores" />
+    <MenuContainer />
     <vue-leaflet />
   </div>
 </template>
@@ -8,24 +8,21 @@
 
 import VueLeaflet from '@c/Leaflet/Leaflet'
 import MenuContainer from '@c/menu/menu'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Main',
-  data () {
-    return {
-      Tw_Stores: []
-    }
-  },
   components: {
     MenuContainer,
     VueLeaflet
   },
   methods: {
+    ...mapActions(['getstroeList']),
     init () {
       fetch('https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json?fbclid=IwAR2faivZHghmapjOcGiuSocqD09wboudZpWjQIfxwG9xCutufqr7Bw06yVk')
         .then(res => res.json())
         .then(jsonData => {
-          this.Tw_Stores = jsonData.features
+          this.getstroeList(jsonData.features)
         })
     }
   },
