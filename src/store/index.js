4b1959c00fa80ae867e1856c80865450b2e9b587
Reaven <y93209@gmail.com>
2020-04-02 18:23:34 +0800

    store add stroeList

--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,37 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    stroeList: [],
     pharmacyList: []
   },
   mutations: {
+    setstroeList (state, payload) {
+      state.stroeList = payload
+    },
     setPharmacyList (state, payload) {
-      state.pharmacyList = payload
+      let data = payload
+      let tw = /(台|臺)../
+      let res = []
+      if (data.match(tw)) {
+        data = data.split('台').join('')
+      }
+      let regExp = new RegExp(data, 'g')
+      res = state.stroeList.filter((features) => {
+        return features.properties.address.match(regExp)
+      })
+      state.pharmacyList = res
     }
   },
   actions: {
+    getstroeList ({ commit }, payload) {
+      commit('setstroeList', payload)
+    },
     getPharmacyList ({ commit }, payload) {
       commit('setPharmacyList', payload)
     }
+  },
+  getters: {
+    statePharmacyList: state => state.pharmacyList
   },
   modules: {
   }
